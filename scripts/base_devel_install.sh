1e4bae885c7eb651010cfeb3e9de1c044bd7a25b
Yonle <yonle@lecturify.net>
2025-06-07 02:11:13 +0700

    scripts/base_devel_install.sh: just add some ingredients
    Signed-off-by: Yonle <yonle@lecturify.net>

--- scripts/base_devel_install.sh
+++ scripts/base_devel_install.sh
@@ -2,5 +2,6 @@
 
 # I wanna do something little bit different here.
 pacman -Sy \
-  auto{conf,make} bison debugedit fakeroot flex gc
-  clang groff guile lib{isl,mpc} m4 make pkgconf texinfo
+  auto{conf,make} bison debugedit fakeroot flex gc \
+  clang groff guile lib{isl,mpc} m4 make pkgconf texinfo \
+  less patch
