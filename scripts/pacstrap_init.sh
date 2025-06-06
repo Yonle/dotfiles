d01d33a1c41dc357568485b076a0dfa469fecd65
Yonle <yonle@lecturify.net>
2025-06-06 22:57:31 +0700

    scripts/pacstrap_init.sh: just let the user pick them later. bruh.
    Signed-off-by: Yonle <yonle@lecturify.net>

--- scripts/pacstrap_init.sh
+++ scripts/pacstrap_init.sh
@@ -5,11 +5,11 @@ pacstrap -K /mnt \
   busybox doas nano helix mandoc \
   iwd networkmanager ly reflector \
   grim slurp brightnessctl libpulse xorg-xwayland wl-clipboard mailcap \
-  gvfs thunar thunar-{volman,archive-plugin} tumbler ffmpegthumbnailer \
+  gvfs thunar tumbler ffmpegthumbnailer \
   foot rofi i3status sway swaybg swayidle swaylock mako \
   pipewire pipewire-alsa pipewire-pulse pipewire-audio \
   xdg-desktop-portal xdg-desktop-portal-wlr wayvnc wf-recorder \
-  mesa-util wayland-util \
+  mesa-utils wayland-utils \
   mpv ffmpeg sox vlc ristretto imv zathura zathura-pdf-poppler \
   htop go git openssh rsync tailscale \
   ttf-{dejavu,jetbrains-mono} wqy-{bitmapfont,microhei,zenhei} noto-fonts-emoji
