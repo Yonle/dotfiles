0347e6bd7bfdc92567e7fef7a2a539f521f6e2ab
Yonle <yonle@lecturify.net>
2025-06-06 22:38:41 +0700

    scripts/pacstrap_init.sh: fix package list

--- scripts/pacstrap_init.sh
+++ scripts/pacstrap_init.sh
@@ -2,14 +2,14 @@
 
 pacstrap -K /mnt \
   base linux-lts linux-firmware intel-ucode limine chrony \
-  busybox doas nano helix man-pages \
+  busybox doas nano helix mandoc \
   iwd networkmanager ly reflector \
   grim slurp brightnessctl libpulse xorg-xwayland wl-clipboard mailcap \
   thunar tumbler ffmpegthumbnailer \
   foot rofi i3status sway swaybg swayidle swaylock mako \
   pipewire pipewire-alsa pipewire-pulse pipewire-audio \
-  xdg-desktop-portal xdg-desktop-portal-wlr wayvnc tailscale \
+  xdg-desktop-portal xdg-desktop-portal-wlr wayvnc wf-recorder \
   mesa-util wayland-util \
   mpv ffmpeg sox vlc ristretto imv zathura zathura-pdf-poppler \
-  htop go git openssh rsync \
+  htop go git openssh rsync tailscale \
   ttf-{dejavu,jetbrains-mono} wqy-{bitmapfont,microhei,zenhei} noto-fonts-emoji
